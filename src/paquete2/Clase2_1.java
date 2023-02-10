a12c1d16a99815c792db13eaaed9f84a8069dbab
Fran <francisconavarro9399@gmail.com>
2023-02-10 16:15:20 +0100

    Primer cambio realizado.

--- src/paquete2/Clase2_1.java
+++ src/paquete2/Clase2_1.java
@@ -5,7 +5,7 @@ public class Clase2_1 {
 	private static void main(String[] args) {
 		
 		System.out.println("Esta es la primera clase del segundo paquete.");
-		System.out.println("Esto es una modificación.");
+		System.out.println("Esto es una modificación");
 		
 	}
 }
