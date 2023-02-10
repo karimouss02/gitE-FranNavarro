0c74d6b5ab940ff1eb5ac9b9fcf99ff86f65e78b
Fran <francisconavarro9399@gmail.com>
2023-02-10 13:16:33 +0100

    Inicio

---
+++ src/paquete1/Clase1_1.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class Clase1_1 {
+	
+	private static void main (String[] args) {
+		
+		System.out.println("Esta es la primera clase del primer paquete.");
+
+	}
+	
+}
---
+++ src/paquete2/Clase2_1.java
@@ -0,0 +1,10 @@
+package paquete2;
+
+public class Clase2_1 {
+	
+	private static void main(String[] args) {
+		
+		System.out.println("Esta es la primera clase del segundo paquete.");
+		
+	}
+}
